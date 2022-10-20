416d43747ffc097fd45cfd6c9291ad237e26e725
Casper <Casper@DESKTOP-E9TV5OK>
2022-10-20 13:07:16 +0300

    Add project files.

---
+++ Workshop3Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> kelimeler = new Dictionary<string, string>();
+            //Ekleme
+            kelimeler.Add("silgi", "erazer");
+            kelimeler.Add("kitap", "book");
+            kelimeler.Add("orange", "portakal");
+            kelimeler.Add("türkçe", "turkish");
+            kelimeler.Add("bilgisayar", "computer");
+            kelimeler.Last();
+            //Foreach ile Gezme
+            foreach (var item in kelimeler)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
